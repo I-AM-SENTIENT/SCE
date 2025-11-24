1ffe2bb6a91f15a019eddaaac72562303a8147e3
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-11-24 03:39:36 +0100

    -

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 class Board:
     def __init__(self):
     #States of the game
-        self.side_to_move = 1 #1 - white, 0 - black
+        self.side_to_move = 0 #0 - white, 1 - black
 
         self.castle_white_short = 1
         self.castle_white_long = 1
@@ -11,8 +11,8 @@ class Board:
         self.en_passant = None #Stores target square
 
         self.half_move_counter = 0 #half-moves since the last pawn advance or capture, half moves means move by 1 player
-        self.full_move_counter = 0 #Full move meaning move by both players
-
+        self.full_move_counter = 1 #Full move meaning move by both players, starts at 1 for some reason, increase after black move
+        
         self.reversible_moves = 0 #Moves by pieces (not pawns) to empty squares, not sure if this usefull
         
 
@@ -20,8 +20,8 @@ class Board:
         self.mailbox120 = [
      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-     -1,  0,  1,  2,  3,  4,  5,  6,  7, -1,
-     -1,  8,  9, 10, 11, 12, 13, 14, 15, -1,
+     -1,  0,  1,  2,  3,  4,  5,  6,  7, -1, #8 row
+     -1,  8,  9, 10, 11, 12, 13, 14, 15, -1, #7 row...
      -1, 16, 17, 18, 19, 20, 21, 22, 23, -1,
      -1, 24, 25, 26, 27, 28, 29, 30, 31, -1,
      -1, 32, 33, 34, 35, 36, 37, 38, 39, -1,
@@ -32,8 +32,8 @@ class Board:
      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
         
         self.mailbox64 = [
-    21, 22, 23, 24, 25, 26, 27, 28,
-    31, 32, 33, 34, 35, 36, 37, 38,
+    21, 22, 23, 24, 25, 26, 27, 28, #8 row
+    31, 32, 33, 34, 35, 36, 37, 38, #7 row...
     41, 42, 43, 44, 45, 46, 47, 48,
     51, 52, 53, 54, 55, 56, 57, 58,
     61, 62, 63, 64, 65, 66, 67, 68,
@@ -42,8 +42,4 @@ class Board:
     91, 92, 93, 94, 95, 96, 97, 98]
         
         #Actual board for storing pieces
-
-        #Initialize it empty, populate from STARTING_FEN
-        self.board = [0] * 64
-
-        
+        self.board_play = [0] * 120
